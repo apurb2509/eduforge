5675356b888924690eebcd8c8ef62e864d104562
Apurb Susobhit Baba <itsapurb@gmail.com>
2026-03-16 22:41:20 +0530

    initialize react frontend with vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <h1>EduForge Frontend Initialized</h1>
+    </div>
+  );
+};
+
+export default App;
